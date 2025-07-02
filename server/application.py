b39fb57fc818bb684b6f68920dedc7e09e5bf307
vyacheslavvankow <vankow@mail.ru>
2025-07-02 13:56:02 +0000

    Решено

--- server/application.py
+++ server/application.py
@@ -6,14 +6,15 @@ PORT = 8000
 
 class TestMe():
     '''For me'''
-    	def take_five(self):
-            return 5
-    	def port(self):
-	    '''port'''
-            return PORT
+    def take_five(self):
+        return 5
+    
+    def port(self):
+        '''port'''
+        return PORT
 
 if __name__ == '__main__':
-    	Handler = http.server.SimpleHTTPRequestHandler
-    	with socketserver.TCPServer(("", PORT), Handler) as http:
-        	print("serving at port", PORT)
-        	http.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
